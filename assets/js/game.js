971c2e63fafc9ba6f362f2ab51e05b43eeec0203
jahilario <josue.a@live.com>
2020-09-16 12:41:36 -0500

    added startGame function (not working)

--- assets/js/game.js
+++ assets/js/game.js
@@ -59,7 +59,6 @@ var fight = function (enemyName) {
         // check player's health
         if (playerHealth <= 0) {
             window.alert(playerName + ' has died!');
-            window.alert("You have lost your robot in battle! Game Over!");
             // leave while() loop if player is dead
             break;
         } else {
@@ -67,12 +66,45 @@ var fight = function (enemyName) {
         }
     }
 };
+//function to start new game
+var startGame = function () {
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 
-for (var i = 0; i < enemyNames.length; i++) {
-    if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-        var pickedEnemyName = enemyNames[i]
-        enemyHealth = 50;
-        fight(pickedEnemyName[i]);
+    for (var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            var pickedEnemyName = enemyNames[i]
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        }
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
     }
+    endGame();
 }
+// function to end the entire game
+var endGame = function () {
+    // if player is still alive, player wins!
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        // restart the game
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+    PAUSE
+}
+
